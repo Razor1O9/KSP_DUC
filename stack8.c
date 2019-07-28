b0c851ad43aab1b65f1e0c02d0880e26203684d9
Duc <duc1991_2011@yahoo.de>
2019-07-28 18:31:04 +0200

    KomandozeileArgumente

---
+++ stack8.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack8.h"
+
+Stackslot stack[SIZE];
+ObjRef *static_data_area;
+// stackpointer
+unsigned int sp = 0;
+// framepointer
+unsigned int fp = 0;
+// ask for funktion with arguments and return value!
+// instruktion popr save in stack or r?
+// return register
+ObjRef *r;
+
+ObjRef newCompoundObject(int objRefSize) {
+    ObjRef objRef = malloc(sizeof(unsigned int) + objRefSize * sizeof(ObjRef));
+    int counter = 0;
+    objRef->size = objRefSize | MSB;
+
+    do{
+        *((ObjRef *)objRef->data + counter++) = NULL;
+    } while(counter < GET_SIZE(objRef));
+
+    return objRef;
+}
+
+int is_objRef(int i) {
+	if(stack[i].isObjRef)
+		return 1;
+	return 0;
+}
+
+void pushNumber(int x) {
+	if(sp >= SIZE) {
+		printf("stackoverflow.\n");
+		exit(1);
+	} else {
+		stack[sp].isObjRef = 0;
+		stack[sp].u.number = x;
+		sp++;
+	}
+}
+
+int popNumber(void) {
+	int pop_wert = stack[sp-1].u.number;
+	if(sp < 0) {
+		printf("Stackunderflow.\n");
+		exit(1);
+	} else {
+		sp--;
+		stack[sp].u.number = 0;
+		stack[sp].u.objRef = NULL;
+	}
+	return pop_wert;
+}
+
+void pushObjRef(ObjRef x) {
+	if(sp >= SIZE) {
+		printf("stackoverflow.\n");
+		exit(1);
+	} else {
+		stack[sp].isObjRef = 1;
+		stack[sp].u.objRef = x;
+		sp++;
+	}
+}
+
+ObjRef popObjRef(void) {
+	ObjRef o = stack[sp-1].u.objRef;
+	if(sp < 0) {
+		printf("Stackunderflow.\n");
+		exit(1);
+	} else {
+		sp--;
+		stack[sp].u.objRef = NULL;
+	}
+	return o;
+}
+
+// push local variable
+void pushl(int location) {
+	pushObjRef(stack[location].u.objRef);
+}
+
+// pop local variable
+void popl(int location) {
+	stack[location].u.objRef = popObjRef();
+}
+
+// push global variable
+void pushg(int location) {
+	pushObjRef(static_data_area[location]);
+}
+
+// pop global variable
+void popg(int location) {
+	static_data_area[location] = popObjRef();
+}
---
+++ stack8.h
@@ -0,0 +1,44 @@
+/*Datei stack.h*/
+
+#ifndef STACK_H
+#define STACK_H
+
+#include <support.h>
+
+//is this object a primitive object?
+#define MSB		(1 << (8 * sizeof(unsigned int) - 1))
+#define IS_PRIM(objRef) (((objRef)->size & MSB) == 0)
+//How many objectrefernce are in this Object
+#define GET_SIZE(objRef) ((objRef)->size & ~MSB)
+//calculate a pointer on the first objectrefernce of object
+#define GET_REFS(objRef) ((ObjRef *)(objRef)->data)
+
+#define SIZE	10000
+
+typedef struct {
+	unsigned int isObjRef;
+	union {
+		ObjRef objRef;
+		int number;
+	} u;
+} Stackslot;
+
+extern Stackslot stack[SIZE];
+extern ObjRef *static_data_area;
+extern unsigned int sp;
+extern unsigned int fp;
+extern ObjRef *r;
+
+int is_objRef(int);
+void pushNumber(int);
+int popNumber(void);
+void pushObjRef(ObjRef x);
+ObjRef popObjRef(void);
+void pushl(int);
+void popl(int);
+void pushg(int);
+void popg(int);
+
+ObjRef newCompoundObject(int numObjRefs);
+
+#endif
