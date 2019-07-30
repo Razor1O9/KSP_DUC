f05be950ca2b5d6c702af28af2c067ebfdc6a36d
Duc <duc1991_2011@yahoo.de>
2019-07-30 14:48:40 +0200

    alter stack8.c

--- stack8.c
+++ stack8.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "stack8.h"
 
 Stackslot *stack;
-char *ziel_halbspeicher;
+char char *ziel_halbspeicher;;
 char *quell_halbspeicher;
 ObjRef *static_data_area;
 // stackpointer
@@ -16,65 +15,10 @@ unsigned int fp = 0;
 // return register
 ObjRef *r;
 int max_size = 0;
-
-ObjRef copyObjectToFreeMem(ObjRef orig);
-
-void garbagecollector();
-
-// ToDo
-/* Garbage Collector starts here */
-ObjRef relocate(ObjRef orig) {
-    ObjRef copy;
-    if (orig == NULL) {
-/* relocate(nil) = nil */
-        copy = NULL;
-    } else if (BROKENHEART(orig)) {
-/* Objekt ist bereits kopiert , Forward -Pointer gesetzt */
-        copy = FORWARDPOINTER(orig); // + (ObjRef heap??)
-    } else {
-/* Objekt muss noch kopiert werden */
-        copy = copyObjectToFreeMem (orig );
-/* im Original: setze Broken -Heart -Flag und Forward -Pointer */
-        orig -> size = SBIT;
-        orig -> size = ((char*)copy - heap | SBIT);
-    }
-/* Adresse des kopierten Objektes zurück */
-    return copy;
-}
-
-ObjRef copyObjectToFreeMem(ObjRef orig) {
-
-    if (!BROKENHEART(orig)) {
-        if (IS_PRIM(orig)) {
-            memcpy(heap + nextPointer, orig, (GET_SIZE(orig) + sizeof(unsigned int)));
-            nextPointer = (GET_SIZE(orig) + sizeof(unsigned int) + nextPointer);
-        } else {
-            memcpy(heap + nextPointer, orig, (sizeof(ObjRef)*GET_SIZE(orig) + sizeof(unsigned int)));
-            nextPointer = (nextPointer + sizeof(ObjRef)*(GET_SIZE(orig) + sizeof(unsigned int))));
-        }
-    }
-    return orig;
-}
-// ToDo replace all malloc (except heap and stack) with allocate
-void *allocate(size_t size){
-    unsigned int halfheapsize;
-    char *temp_heap;
-    temp_heap = heap + nextPointer;
-    nextPointer += size;
-    if(temp_heap >=  heap + halfheapsize){
-        garbagecollector();
-    }
-    return temp_heap;
-}
-
-void garbagecollector() {
-
-}
-/* Garbage Collector ends here */
+unsigned int nextPointer = 0;
 
 ObjRef newCompoundObject(int objRefSize) {
-    //ObjRef objRef = malloc(sizeof(unsigned int) + objRefSize * sizeof(ObjRef));
-    ObjRef objRef = allocate(objRefSize * sizeof(ObjRef) + sizeof(unsigned int));
+    ObjRef objRef = allocate(sizeof(unsigned int) + objRefSize * sizeof(ObjRef));
     objRef->size = objRefSize | MSB;
 
     for(int i = 0; i < objRefSize; i++)
@@ -84,74 +28,74 @@ ObjRef newCompoundObject(int objRefSize) {
 }
 
 int is_objRef(int i) {
-	if(stack[i].isObjRef)
-		return 1;
-	return 0;
+    if(stack[i].isObjRef)
+        return 1;
+    return 0;
 }
 
 void pushNumber(int x) {
-	if(sp >= max_size) {
-		printf("stackoverflow.\n");
-		exit(1);
-	} else {
-		stack[sp].isObjRef = 0;
-		stack[sp].u.number = x;
-		sp++;
-	}
+    if(sp >= max_size) {
+        printf("stackoverflow.\n");
+        exit(1);
+    } else {
+        stack[sp].isObjRef = 0;
+        stack[sp].u.number = x;
+        sp++;
+    }
 }
 
 int popNumber(void) {
-	int pop_wert = stack[sp-1].u.number;
-	if(sp < 0) {
-		printf("Stackunderflow.\n");
-		exit(1);
-	} else {
-		sp--;
-		stack[sp].u.number = 0;
-		stack[sp].u.objRef = NULL;
-	}
-	return pop_wert;
+    int pop_wert = stack[sp-1].u.number;
+    if(sp < 0) {
+        printf("Stackunderflow.\n");
+        exit(1);
+    } else {
+        sp--;
+        stack[sp].u.number = 0;
+        stack[sp].u.objRef = NULL;
+    }
+    return pop_wert;
 }
 
 void pushObjRef(ObjRef x) {
-	if(sp >= max_size) {
-		printf("stackoverflow.\n");
-		exit(1);
-	} else {
-		stack[sp].isObjRef = 1;
-		stack[sp].u.objRef = x;
-		sp++;
-	}
+    if(sp >= max_size) {
+        printf("stackoverflow.\n");
+        exit(1);
+    } else {
+        stack[sp].isObjRef = 1;
+        stack[sp].u.objRef = x;
+        sp++;
+    }
 }
 
 ObjRef popObjRef(void) {
-	ObjRef o = stack[sp-1].u.objRef;
-	if(sp < 0) {
-		printf("Stackunderflow.\n");
-		exit(1);
-	} else {
-		sp--;
-		stack[sp].u.objRef = NULL;
-	}
-	return o;
+    ObjRef o = stack[sp-1].u.objRef;
+    if(sp < 0) {
+        printf("Stackunderflow.\n");
+        exit(1);
+    } else {
+        sp--;
+        stack[sp].u.objRef = NULL;
+    }
+    return o;
 }
 
 // push local variable
 void pushl(int location) {
-	pushObjRef(stack[location].u.objRef);
+    pushObjRef(stack[location].u.objRef);
 }
 
 // pop local variable
 void popl(int location) {
-	stack[location].u.objRef = popObjRef();
+    stack[location].u.objRef = popObjRef();
 }
 
 // push global variable
 void pushg(int location) {
-	pushObjRef(static_data_area[location]);
+    pushObjRef(static_data_area[location]);
 }
 
 // pop global variable
 void popg(int location) {
-	static_data_area[location] = popObjRef();
+    static_data_area[location] = popObjRef();
 }
