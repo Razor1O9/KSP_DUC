c889b23dd82e057c8342462993ac7fc79649c4e1
David <david_omran@yahoo.de>
2019-08-02 10:32:14 +0200

    new folder ydom20

--- ydom20/stack8.c
+++ ydom20/stack8.c
@@ -18,6 +18,7 @@ unsigned int nextPointer = 0;
 
 char *ziel_halbspeicher;
 char *quell_halbspeicher;
+char *heap;
 unsigned int set_stack_size = 0;
 unsigned int set_heap_size = 0;
 unsigned int halfsize = 0;
@@ -26,164 +27,113 @@ header_t buffer;
 
 char *temp_heap;
 
+int button = 1;
+
 ObjRef copyObjectToFreeMem(ObjRef orig){
-	if(!BROKEN_HEART(orig)){
-		printf("BROKEN_HEART\n");
 		if(IS_PRIM(orig)){
-			printf("IS_PRIM\n");
-			printf("nextPointer = %d und ziel_halbspeicher = %p und orig = %p\n", nextPointer, ziel_halbspeicher + nextPointer, (void*) orig);
 			memcpy((ziel_halbspeicher + nextPointer), orig, (GET_SIZE(orig) + sizeof(unsigned int)));
-			printf("nach IS_PRIM\n");
 			nextPointer += (GET_SIZE(orig) + sizeof(unsigned int));
 		}
 		else{
-			printf("not IS_PRIM\n");
-
 			memcpy(ziel_halbspeicher + nextPointer, orig, (sizeof(ObjRef)*(GET_SIZE(orig) + sizeof(unsigned int))));
-
 			nextPointer += (sizeof(ObjRef)*(GET_SIZE(orig) + sizeof(unsigned int)));
-
-			printf("nach not IS_PRIM\n");
-
 		}
-	}
-	printf("nach copy\n");
 	return orig;
 }
 
 ObjRef relocate(ObjRef orig) {
-	printf("relocate\n");
 	ObjRef copy;
 	if(orig == NULL) {
-		printf("orig = NULL\n");
-
 		copy = NULL;
 	}
 	else if((orig->size & SECBIT) == 1) {
-		printf("realocate BROKEN_HEART\n");
-
-        copy->size = FORWARDPOINTER(orig);
+		copy->size = FORWARDPOINTER(orig);
 	}
 	else
 	{
-		printf("realocate else = %p\n", (void*) orig);
-
 		copy = copyObjectToFreeMem(orig);
-
+		
 		orig->size = SECBIT;
 		orig->size = FORWARDPOINTER(copy);
 	}
-	printf("nach relocate\n");
 	return copy;
 }
 
 void garbagecollector(){
 	char* scan = 0;
-	int i = 0;
-	int k = 0;
-	int j = 0;
 	char *temp;
 	temp = ziel_halbspeicher;
 	ziel_halbspeicher = quell_halbspeicher;
 	quell_halbspeicher = temp;
 	nextPointer = 0;
-
-	for(j = 0; j < buffer.sda; j++){
-		static_data_area[j] = relocate(static_data_area[j]);
+	for(int i = 0; i < buffer.sda; i++){
+		static_data_area[i] = relocate(static_data_area[i]);
 	}
-	printf("static_data_area\n");
-
 	r[1] = relocate(r[1]);
-	printf("returnregister\n");
-
-	printf("vor stack\n");
-	for(i = 0; i < sp; i++){
-		if(stack[i].isObjRef){
-			stack[i].u.objRef = relocate(stack[i].u.objRef);
-			printf("stack[i].u.objRef = %p\n", (void*) stack[i].u.objRef);
+	for(int j = 0; j < sp; j++){
+		if(stack[j].isObjRef){
+			stack[j].u.objRef = relocate(stack[j].u.objRef);
 		}
 	}
-	printf("nach stack\n");
 	
 	bip.op1 = relocate(bip.op1);
 	bip.op2 = relocate(bip.op2);
 	bip.res = relocate(bip.res);
 	bip.rem = relocate(bip.rem);
-	printf("bip\n");
 
 	scan = ziel_halbspeicher;
-	while(scan <= ziel_halbspeicher + nextPointer) {
-/* es gibt noch Objekte, die gescannt werden müssen */
+	while(scan != ziel_halbspeicher + nextPointer) {
 		if(!IS_PRIM((ObjRef)scan)) {
-			for(k = 0; k < GET_SIZE((ObjRef)scan); k++) {
+			for(int k = 0; k < GET_SIZE((ObjRef)scan); k++) {
 				GET_REFS((ObjRef)scan)[k] = relocate(GET_REFS((ObjRef)scan)[k]);
 			}
-			printf("end of for\n");
-		//scan += GET_SIZE((ObjRef)scan) * 8 + sizeof(unsigned int);
 		}
-		printf("scan %ld\n", (GET_SIZE((ObjRef)scan) + sizeof(unsigned int)));
-		//else{
 		scan +=  (GET_SIZE((ObjRef)scan) + sizeof(unsigned int));
-		//}
-		printf("scan %p\n", (void*) scan);
-		printf("ziel_halbspeicher %p\n", (void*) ziel_halbspeicher);
 	}
 	nextPointer = 0;
-	printf("end while\n");
+	if(button)
+		button = 0;
+	else
+		button = 1;
 }
 
 void *allocate_header(size_t size) {
 	temp_heap = ziel_halbspeicher + nextPointer;
 	nextPointer += size;
 	if(nextPointer >= halfsize) {
-        printf("Ninja Virtual Machine started\n");
+		printf("Ninja Virtual Machine started\n");
 		return temp_heap = NULL;
 	}
-	printf("zu klein\n");
 	return temp_heap;
 }
 
 void *allocate_data(size_t size){
 	char *x;
-	/*printf("ziel_halbspeicher = %p\n", ziel_halbspeicher);
-	printf("ziel_halbspeicher + nextPointer = %p\n", ziel_halbspeicher + nextPointer);
-	printf("ziel_halbspeicher + halfsize = %p\n", ziel_halbspeicher + halfsize);*/
-	//temp_heap = ziel_halbspeicher;
-	//temp_heap = temp_heap + + nextPointer;
-	printf("nextPointer = %d\n", nextPointer);
-	temp_heap = ziel_halbspeicher + nextPointer;
-	nextPointer += size;
-	//printf("Vergleich von Zeigern = %d\n", temp_heap >= (ziel_halbspeicher + halfsize));
-	/*if(temp_heap >= (ziel_halbspeicher + halfsize)){
-		printf("gc\n");
-	
-		printf("garbagecollector11111111111111111111111111111111111111111111111111111111111111111111\n");
-		garbagecollector();
-	}*/
-	x = temp_heap;
-	//printf("Vergleich von Zeigern 2 = %d\n", (temp_heap + size) >= (ziel_halbspeicher + halfsize));
-	if((x) >= (ziel_halbspeicher + halfsize)) {
-		printf("gc\n");
-		garbagecollector();	
-	} //else if(nextPointer >= halfsize) {
-		//	return temp_heap = NULL;
-	//printf("temp_heap = %p\n", temp_heap);
-	//printf("ziel_halbspeicher + halfsize = %p\n", ziel_halbspeicher + halfsize);
-	//}
-	//printf("nextPointer >= halfsize = %d\n", nextPointer >= halfsize);
-	//printf("halfsize = %d\n", halfsize);
-	//printf("temp_heap = %p\n", temp_heap);
-	
+	if(button) {
+		temp_heap = ziel_halbspeicher + nextPointer;
+		nextPointer += size;	
+		x = temp_heap;
+		if(x + size > quell_halbspeicher) {
+			garbagecollector();	
+		}
+	} else {
+		temp_heap = ziel_halbspeicher - nextPointer;
+		nextPointer += size;	
+		x = temp_heap;
+		if(x - size < quell_halbspeicher) {
+			garbagecollector();	
+		}
+	} 
 	return temp_heap;
 }
 
 ObjRef newCompoundObject(int objRefSize) {
-    	ObjRef objRef = allocate_data(sizeof(unsigned int) + objRefSize * sizeof(ObjRef));
+    	ObjRef objRef = malloc(sizeof(unsigned int) + objRefSize * sizeof(ObjRef));
 	objRef->size = objRefSize | MSB;
 	if (objRef == NULL) {
    		 fatalError("newCompoundObject() got no memory");
   	}
-	for(unsigned int i = 0; i < GET_SIZE(objRef); i++)
+	for(int i = 0; i < objRefSize; i++)
 		GET_REFS(objRef)[i] = NULL;
 
     return objRef;
